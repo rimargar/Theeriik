fff56bab91a1347decf76a42426395c58ae1dc8a
rimargar <98271344+rimargar@users.noreply.github.com>
2025-10-14 12:52:44 +0200

    Create scrips.sh

---
+++ scrips.sh
@@ -0,0 +1,9 @@
+sudo dnf -y install docbook-style-xsl gcc gdb gnutls-devel gpgme-devel \
+ jansson-devel \
+ keyutils-libs-devel krb5-workstation libacl-devel libaio-devel \
+ libarchive-devel libattr-devel libblkid-devel libtasn1 libtasn1-tools \
+ libxml2-devel libxslt lmdb-devel openldap-devel pam-devel perl \
+ perl-ExtUtils-MakeMaker perl-Parse-Yapp popt-devel python3-cryptography \
+ python3-dns python3-gpg python3-devel readline-devel rpcgen systemd-devel \
+ tar zlib-devel perl-JSON \
+ libtirpc-devel dbus-devel python3-pyasn1 python3-markdown
